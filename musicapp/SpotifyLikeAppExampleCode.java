1a615507024e0380b413841da4043aee3634e2ee
Dustin <vlopezklein@outlook.com>
2022-11-03 12:33:37 -0700

    Created Java Maven Project and Added JSON
    Created Java project using Maven quickstart-archetype and added JSON dependency.

---
+++ musicapp/SpotifyLikeAppExampleCode.java
@@ -0,0 +1,185 @@
+package com.example;
+
+import static javax.sound.sampled.AudioSystem.*;
+
+import java.io.*;
+import java.util.*;
+import javax.sound.sampled.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+/*
+    To compile: javac SpotifyLikeApp.java
+    To run: java SpotifyLikeApp
+ */
+
+// declares a class for the app
+public class SpotifyLikeAppExampleCode {
+
+  // global variables for the app
+  String status;
+  Long position;
+  static Clip audioClip;
+
+  /*
+    *** IMPORTANT NOTE FOR ALL STUDENTS *******
+
+    This next line of code is a "path" that students will need to change in order to play music on their
+    computer.  The current path is for my laptop, not yours.
+    
+    If students who do not understand whre files are located on their computer or how paths work on their computer, 
+    should immediately complete the extra credit on "Folders and Directories" in the canvas modules.  
+    
+    Knowing how paths work is fundamental knowledge for using a computer as a technical person.
+
+    Students who do not know what a path are often not able complete this assignment succesfullly.  Please
+    do the extra credit if you are confused. :)  
+    
+    Thank you!  -Gabriel
+
+    */
+
+  private static String basePath =
+    "C:/Users/jerom/Documents/GitHub/class-java/spotify-like-app/demo/src/main/java/com/example/";
+
+  // "main" makes this class a java app that can be executed
+  public static void main(final String[] args) {
+    // reading audio library from json file
+    JSONArray library = readAudioLibrary();
+
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    String userInput = "";
+    while (!userInput.equals("q")) {
+      menu();
+
+      // get input
+      userInput = input.nextLine();
+
+      // accept upper or lower case commands
+      userInput = userInput.toLowerCase();
+
+      // do something
+      handleMenu(userInput, library);
+    }
+
+    // close the scanner
+    input.close();
+  }
+
+  /*
+   * displays the menu for the app
+   */
+  public static void menu() {
+    System.out.println("---- SpotifyLikeApp ----");
+    System.out.println("[H]ome");
+    System.out.println("[S]earch by title");
+    System.out.println("[L]ibrary");
+    System.out.println("[P]lay");
+    System.out.println("[Q]uit");
+
+    System.out.println("");
+    System.out.print("Enter q to Quit:");
+  }
+
+  /*
+   * handles the user input for the app
+   */
+  public static void handleMenu(String userInput, JSONArray library) {
+    switch (userInput) {
+      case "h":
+        System.out.println("-->Home<--");
+        break;
+      case "s":
+        System.out.println("-->Search by title<--");
+        break;
+      case "l":
+        System.out.println("-->Library<--");
+        break;
+      case "p":
+        System.out.println("-->Play<--");
+        play(library);
+        break;
+      case "q":
+        System.out.println("-->Quit<--");
+        break;
+      default:
+        break;
+    }
+  }
+
+  /*
+   * plays an audio file
+   */
+  public static void play(JSONArray library) {
+    // open the audio file
+
+    // get the filePath and open a audio file
+    final Integer songIndex = 3;
+    JSONObject obj = (JSONObject) library.get(songIndex);
+    final String filename = (String) obj.get("filename");
+    final String filePath = basePath + "/wav/" + filename;
+    final File file = new File(filePath);
+
+    // stop the current song from playing, before playing the next one
+    if (audioClip != null) {
+      audioClip.close();
+    }
+
+    try {
+      // create clip
+      audioClip = AudioSystem.getClip();
+
+      // get input stream
+      final AudioInputStream in = AudioSystem.getAudioInputStream(file);
+
+      audioClip.open(in);
+      audioClip.setMicrosecondPosition(0);
+      audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  //
+  // Func: readJSONFile
+  // Desc: Reads a json file storing an array and returns an object
+  // that can be iterated over
+  //
+  public static JSONArray readJSONArrayFile(String fileName) {
+    // JSON parser object to parse read file
+    JSONParser jsonParser = new JSONParser();
+
+    JSONArray dataArray = null;
+
+    try (FileReader reader = new FileReader(fileName)) {
+      // Read JSON file
+      Object obj = jsonParser.parse(reader);
+
+      dataArray = (JSONArray) obj;
+      // System.out.println(dataArray);
+
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+
+    return dataArray;
+  }
+
+  // read the audio library of music
+  public static JSONArray readAudioLibrary() {
+    final String jsonFileName = "audio-library.json";
+    final String filePath = basePath + "/" + jsonFileName;
+
+    JSONArray jsonData = readJSONArrayFile(filePath);
+
+    System.out.println("Reading the file " + filePath);
+
+    return jsonData;
+  }
+}
